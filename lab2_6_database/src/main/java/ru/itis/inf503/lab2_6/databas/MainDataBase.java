ba908a691bd85a4cc60e02201c992f2008deaf09
ksenikeev <ksenikeev@mail.ru>
2026-04-11 13:36:08 +0300

    database

--- lab2_6_database/src/main/java/ru/itis/inf503/lab2_6/databas/MainDataBase.java
+++ lab2_6_database/src/main/java/ru/itis/inf503/lab2_6/databas/MainDataBase.java
@@ -5,13 +5,21 @@ import java.util.Date;
 public class MainDataBase {
     public static void main(String[] args) {
         try {
-            Many many1 = new Many(4, (byte)-1, "Автобус", new Date(),46f,  "");
-            Many many2 = new Many(3, (byte)-1, "Троллейбус", new Date(),46f,  "");
+//            Many many1 = new Many(4, (byte)-1, "Автобус", new Date(),46f,  "");
+//            Many many2 = new Many(3, (byte)-1, "Троллейбус", new Date(),46f,  "");
 
             DataBaseUtils dataBaseUtils = new DataBaseUtils();
 
-            dataBaseUtils.save(many1);
-            dataBaseUtils.save(many2);
+//            dataBaseUtils.save(many1);
+//            dataBaseUtils.save(many2);
+
+//            Many many3 = dataBaseUtils.findById(4);
+//            System.out.println(many3);
+//
+//            dataBaseUtils.deleteById(4);
+//
+//            many3 = dataBaseUtils.findById(4);
+//            System.out.println(many3);
 
             Many many3 = dataBaseUtils.findById(3);
             System.out.println(many3);
