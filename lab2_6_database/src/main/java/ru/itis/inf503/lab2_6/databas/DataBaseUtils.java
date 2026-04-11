d253c92a176890ca503c7d7ae5a120a01d8d6c0a
ksenikeev <ksenikeev@mail.ru>
2026-04-11 13:25:31 +0300

    database

--- lab2_6_database/src/main/java/ru/itis/inf503/lab2_6/databas/DataBaseUtils.java
+++ lab2_6_database/src/main/java/ru/itis/inf503/lab2_6/databas/DataBaseUtils.java
@@ -5,30 +5,80 @@ import java.io.*;
 public class DataBaseUtils {
     private static final String FILE_NAME = "many.tbl";
     public void save(Many many) throws IOException {
-        FileOutputStream fos = null;
-        ObjectOutputStream os = null;
-        DataOutputStream dos = null;
-        try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            os = new ObjectOutputStream(bos);
+
+        // используем вариант try с ресурсами, который
+        // обеспечит автоматическое закрытие перечисленных ресурсов
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream os = new ObjectOutputStream(bos);
+             FileOutputStream fos = new FileOutputStream(FILE_NAME, true);
+             DataOutputStream dos = new DataOutputStream(fos)
+        ) {
+            // Сериализуем объект в ByteArrayOutputStream
             os.writeObject(many);
+            // из ByteArrayOutputStream извлекаем массив, содержащий объект many
             byte[] array = bos.toByteArray();
 
-            fos = new FileOutputStream(FILE_NAME, true);
-            dos = new DataOutputStream(fos);
-
+            // пишем id записи
             dos.writeInt(many.getId());
+            // пишем 1, т.е. запись актуальна (не удалена)
             dos.writeByte(1);
+            // пишем размер сериализоанного объекта
             dos.writeInt(array.length);
+            // пишем объект в сериализованном виде
             dos.write(array);
-        } finally {
-            if (dos != null) {
-                dos.close();
-            }else if (fos != null){
-                fos.close();
+        }
+    }
+
+    public Many findById(Integer id) throws Exception {
+        Many result = null;
+
+        try(DataInputStream dis = new DataInputStream(new FileInputStream(FILE_NAME))){
+            while (dis.available() > 0) {
+                int n = dis.readInt();
+                int flag = dis.readByte();
+                int size = dis.readInt();
+                if (flag == 1 && n == id) {
+                    byte[] buffer = new byte[size];
+                    dis.readFully(buffer);
+                    ObjectInputStream os = new ObjectInputStream(new ByteArrayInputStream(buffer));
+                    result = (Many) os.readObject();
+                    break;
+                } else {
+                    dis.skipBytes(size);
+                }
+            }
+        }
+        return result;
+    }
+
+    public void deleteById(Integer id) throws Exception {
+        // Храним позицию в которой находится нужная запись
+        int position = 0;
+        // Флаг, показывающий нашли или нет нужную запись
+        boolean find = false;
+
+        try(DataInputStream dis = new DataInputStream(new FileInputStream(FILE_NAME))){
+            while (dis.available() > 0) {
+                int n = dis.readInt();
+                int flag = dis.readByte();
+                int size = dis.readInt();
+                if (flag == 1 && n == id) {
+                    find = true;
+                    break;
+                } else {
+                    dis.skipBytes(size);
+                }
+                position += 4 + 1 + 4 + size;
             }
         }
 
-
+        if (find) {
+            // RandomAccessFile - класс для чтения/записи файла в определенной позиции
+            try (RandomAccessFile raf = new RandomAccessFile(FILE_NAME, "rw")) {
+                long offset = position + 4; // Позиция, где нужно изменить байт
+                raf.seek(offset); // Перемещаемся на нужную позицию
+                raf.writeByte(0); // Записываем байт со значением 0
+            }
+        }
     }
 }
--- lab2_6_database/src/main/java/ru/itis/inf503/lab2_6/databas/MainDataBase.java
+++ lab2_6_database/src/main/java/ru/itis/inf503/lab2_6/databas/MainDataBase.java
@@ -5,10 +5,16 @@ import java.util.Date;
 public class MainDataBase {
     public static void main(String[] args) {
         try {
-            Many many1 = new Many(1, (byte)-1, "Автобус", new Date(),46f,  "");
+            Many many1 = new Many(4, (byte)-1, "Автобус", new Date(),46f,  "");
+            Many many2 = new Many(3, (byte)-1, "Троллейбус", new Date(),46f,  "");
+
             DataBaseUtils dataBaseUtils = new DataBaseUtils();
 
             dataBaseUtils.save(many1);
+            dataBaseUtils.save(many2);
+
+            Many many3 = dataBaseUtils.findById(3);
+            System.out.println(many3);
         }catch (Exception e) {
             e.printStackTrace();
         }
