3180b44fbf1e2852e3e1e87cb7ab1a93db689c2f
ksenikeev <ksenikeev@mail.ru>
2026-05-18 12:52:08 +0300

    task

--- lab2_12_parallel_work/src/main/java/ru/itis/inf503/lab2_12/task20260518/RandomGeneratorTask.java
+++ lab2_12_parallel_work/src/main/java/ru/itis/inf503/lab2_12/task20260518/RandomGeneratorTask.java
@@ -3,12 +3,13 @@ package ru.itis.inf503.lab2_12.task20260518;
 public class RandomGeneratorTask extends Thread {
 
     // методы для управления
-    // setMax
-    // getRandom
+    // synchronized setMax
+    // synchronized getRandom
     // stop
     // start
 
     public void run() {
-
+        // Ссылка на текущий поток
+        Thread.currentThread();
     }
 }
